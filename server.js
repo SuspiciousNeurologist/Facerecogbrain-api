ef200984672fee99b36c5a4812170ebf88452d9b
AndreW <aldape.andres2@gmail.com>
2020-04-01 21:34:51 -0600

    Changning res

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/',(req,res)=>{res.send('It is working!')})
+app.get('/',(req,res)=>{res.send('It is working!')})
  
 app.post('/signin', signin.handleSignIn(db,bcrypt));
 
